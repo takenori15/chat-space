b018468a83ea2c7a367bf14d7c602f494a880b33
takenori15 <tak2kk@gmail.com>
2017-09-09 19:05:32 +0900

    Generate models with index
    Groupモデルを作成した後にindexを追加するのではなく、モデル生成時に一括で行った。

--- db/migrate/20170909095431_create_groups.rb
+++ db/migrate/20170909095431_create_groups.rb
@@ -2,7 +2,9 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :group_name, null: false, unique: true
+
       t.timestamps
     end
+    add_index :groups, :group_name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909085351) do
+ActiveRecord::Schema.define(version: 20170909100036) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "group_name", null: false
