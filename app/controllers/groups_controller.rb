8bc3ed2c46fe63638e74244a1f1f680adfbd7312
takenori15 <tak2kk@gmail.com>
2017-09-11 17:51:42 +0900

    Edit controller and view file to create a record in groups table

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,23 @@
 class GroupsController < ApplicationController
+
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(group_params)
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:group_name, {:user_ids => []})
   end
 
 end
