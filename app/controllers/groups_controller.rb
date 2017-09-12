9a9afba32507c23ec4bf57a53afde1d4593e89a4
takenori15 <tak2kk@gmail.com>
2017-09-12 14:56:23 +0900

    Changes for hash rules

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to :root, :notice => "グループの作成に成功しました！"
+      redirect_to :root, notice: "グループの作成に成功しました！"
     else
       render :new
     end
@@ -22,7 +22,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:group_name, {:user_ids => []})
+    params.require(:group).permit(:group_name, {user_ids: []})
   end
 
 end
