8e25306b45a9e4f1636929846905fe0ccb1e7bfd
takenori15 <tak2kk@gmail.com>
2017-09-13 20:04:47 +0900

    Edit controller and view file then fix details in css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,18 @@ class MessagesController < ApplicationController
   def index
     @user = current_user
     @groups = current_user.groups
+    @message = Message.new
+    @group = Group.find(params[:group_id])
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @message = Message.new(body: message_params[:body], image: message_params[:image], group_id: @group.id, user_id: current_user.id)
+    @message.save
+  end
 
+  private
+  def message_params
+    params.require(:message).permit(:body, :image)
   end
 end
