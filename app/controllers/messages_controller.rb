d15a20f5f67c730f4f6701efebed8cc334fb69f0
takenori15 <tak2kk@gmail.com>
2017-09-13 20:58:39 +0900

    Write another before_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,12 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group
   def index
     @groups = current_user.groups
     @message = Message.new
-    @group = Group.find(params[:group_id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @message = Message.new(body: message_params[:body], image: message_params[:image], group_id: @group.id, user_id: current_user.id)
     @message.save
   end
@@ -15,5 +14,9 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:body, :image)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
