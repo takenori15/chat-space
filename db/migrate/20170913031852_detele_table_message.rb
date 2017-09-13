dc3d0f8d302468e6ab454bb3458846ff3b93df58
takenori15 <tak2kk@gmail.com>
2017-09-13 13:09:13 +0900

    Generate model again

---
+++ db/migrate/20170913031852_detele_table_message.rb
@@ -0,0 +1,5 @@
+class DeteleTableMessage < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :messages
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912115130) do
+ActiveRecord::Schema.define(version: 20170913032246) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "group_name", null: false
