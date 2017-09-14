8ee8c9c127cba4237e9dd0c4e93f1fd5632f5511
takenori15 <tak2kk@gmail.com>
2017-09-14 11:26:31 +0900

    Add validation
    ビューの分岐を確かめるために追記。

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validate :two_values_should_be_included
+
+  def two_values_should_be_included
+    condition = body && image
+    errors.add(:body, "どちらの値も入力されていません！") if condition.blank?
+  end
 end
